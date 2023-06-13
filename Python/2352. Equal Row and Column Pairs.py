04f5caf4ba52349fce9a2ebf611f914107bb435b
Ando Tiana <rktandoscound@gmail.com>
2023-06-13 12:36:10 +0300

    Add python leetcode solution

---
+++ Python/2352. Equal Row and Column Pairs.py
@@ -0,0 +1,5 @@
+from collections import Counter    
+    
+def equalPairs(self, grid: List[List[int]]) -> int:
+    cnt = Counter('|'.join(str(_x) for _x in row) for row in grid)
+    return sum(cnt['|'.join(str(_y) for _y in row)] for row in zip(*grid))
---
+++ Python/2353. Design a Food Rating System.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+import heapq
+
+
+# FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
+
+
+class FoodRatings:
+
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        self._dict = defaultdict(list)
+        self._food_rating = {}
+        self._food_cuisine = {}
+        for f, r, c in zip(foods, ratings, cuisines):
+            heapq.heappush(self._dict[c], (-r, f))
+            self._food_cuisine[f] = c
+            self._food_rating[f] = r
+
+
+    def changeRating(self, food: str, new_rating: int) -> None:
+        cuisine = self._food_cuisine[food]
+        self._food_rating[food] = new_rating
+        heapq.heappush(self._dict[cuisine], (-new_rating, food))
+
+
+    def highestRated(self, cuisine: str) -> str:
+        curr = self._dict[cuisine]
+        while curr:
+            rating, food = curr[0]
+            rating = -rating
+            if rating == self._food_rating[food]:
+                return food
+            heapq.heappop(curr)
+        return ''
